8af44fbe90c8a45e97ea56db3476f5415fe98fb5
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-09-16 18:17:55 +0300

    add jwt authentication

--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    model = get_user_model()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
